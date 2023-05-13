fcc84a62df85bf4d6987e1f713b195bd37badf4f
Mertcan Poyraz <mertcan_1999@icloud.com>
2023-05-13 21:34:58 +0300

    added to the comment for StreamHashMapFilter class

--- src/main/java/org/example/StreamHashMapFilter.java
+++ src/main/java/org/example/StreamHashMapFilter.java
@@ -5,6 +5,11 @@ import java.util.stream.Collectors;
 
 public class StreamHashMapFilter {
 
+    /**
+     * Kullanıcıların adlarını ve yaşlarını dönen bir response'umuz olduğunu düşünelim.
+     * Daha sonrasında da HashMap'imizin içine girelim ve dönen response'taki value(age) > 21'den büyükse bu kullanıcıları filtreleyip bir listeye koysun ve daha sonrasında bu oluşan listenin outputunu görelim.
+     * @param args
+     */
     public static void main(String[] args) {
 
         Map<String, Integer> ages = new HashMap<>();
